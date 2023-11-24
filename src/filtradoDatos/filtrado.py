efc1e7f7a29f77bf70f197e6aa2f9864f34b460a
Felipe Ulloa <felipeulloa5a43@gmail.com>
2023-11-24 20:25:03 -0300

    feat: separar excel por horario

--- src/filtradoDatos/filtrado.py
+++ src/filtradoDatos/filtrado.py
@@ -1,25 +1,51 @@
 import pandas as pd
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
+import datetime
+
+
+def guardar_excel(df, nombre_archivo):
+    wb = Workbook()
+    ws = wb.active
+    for r in dataframe_to_rows(df, index=False, header=True):
+        ws.append(r)
+
+    ws.column_dimensions['A'].width = 15
+    ws.column_dimensions['B'].width = 15
+    ws.column_dimensions['C'].width = 20
+    ws.column_dimensions['D'].width = 20
+
+    wb.save(nombre_archivo)
+
+
+def convertir_hora(hora):
+    try:
+        return pd.to_datetime(hora, format='%H:%M:%S').time()
+    except ValueError:
+        return None
+
 
 archivo_xls = '__RESUMEN DE SERVICIOS - 2022.xlsx'
-
 df = pd.read_excel(archivo_xls, header=3)
-df_filtrado = df[df['FECHA'].notna() & df['HORA'].notna(
-) & df['ORIGEN'].notna() & df['DESTINO'].notna()].copy()
-df_filtrado.loc[:, 'FECHA'] = pd.to_datetime(df_filtrado['FECHA']).dt.date
+
+df['FECHA'] = pd.to_datetime(df['FECHA']).dt.date
+df['HORA'] = df['HORA'].apply(convertir_hora)
 
 columnas_a_mostrar = ['FECHA', 'HORA', 'ORIGEN', 'DESTINO']
-df_filtrado = df_filtrado[columnas_a_mostrar]
 
-wb = Workbook()
-ws = wb.active
+df_filtrado = df.dropna(subset=columnas_a_mostrar)
 
-for r in dataframe_to_rows(df_filtrado, index=False, header=True):
-    ws.append(r)
+horario_punta = df_filtrado[df_filtrado['HORA'].between(datetime.time(6, 0), datetime.time(8, 0)) |
+                            df_filtrado['HORA'].between(datetime.time(19, 0), datetime.time(23, 0))]
 
-ws.column_dimensions['A'].width = 15
-ws.column_dimensions['C'].width = 30
-ws.column_dimensions['D'].width = 30
+horario_bajo = df_filtrado[~df_filtrado['HORA'].between(datetime.time(6, 0), datetime.time(8, 0)) &
+                           ~df_filtrado['HORA'].between(datetime.time(19, 0), datetime.time(23, 0))]
 
-wb.save('viajes_filtrados.xlsx')
+guardar_excel(horario_punta[columnas_a_mostrar], 'viajes_horario_punta.xlsx')
+guardar_excel(horario_bajo[columnas_a_mostrar], 'viajes_horario_bajo.xlsx')
+
+print(f"Filas en el archivo Excel original: {len(df)}")
+print(f"Filas en el archivo Excel de horario punta: {len(horario_punta)}")
+print(f"Filas en el archivo Excel de horario bajo: {len(horario_bajo)}")
+print(
+    f"Filas filtradas (no incluidas en horario punta o bajo): {len(df) - len(horario_punta) - len(horario_bajo)}")
