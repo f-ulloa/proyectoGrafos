aa756364b0f3446f84d8bfb813176f5bc9fb23e1
Felipe Ulloa <felipeulloa5a43@gmail.com>
2023-11-24 20:45:33 -0300

    feat: separar los viajes por los escenarios

--- src/filtradoDatos/filtrado.py
+++ src/filtradoDatos/filtrado.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
-import datetime
 
 
 def guardar_excel(df, nombre_archivo):
@@ -16,11 +15,14 @@ def guardar_excel(df, nombre_archivo):
     ws.column_dimensions['D'].width = 20
 
     wb.save(nombre_archivo)
+    print(f"Viajes en {nombre_archivo}: {len(df)}")
+    return len(df)
 
 
 def convertir_hora(hora):
     try:
-        return pd.to_datetime(hora, format='%H:%M:%S').time()
+        hora_convertida = pd.to_datetime(hora, format='%H:%M:%S').time()
+        return hora_convertida.strftime('%H:%M')
     except ValueError:
         return None
 
@@ -28,24 +30,30 @@ def convertir_hora(hora):
 archivo_xls = '__RESUMEN DE SERVICIOS - 2022.xlsx'
 df = pd.read_excel(archivo_xls, header=3)
 
-df['FECHA'] = pd.to_datetime(df['FECHA']).dt.date
+df['FECHA'] = pd.to_datetime(df['FECHA'])
+df['Mes'] = df['FECHA'].dt.month
 df['HORA'] = df['HORA'].apply(convertir_hora)
 
 columnas_a_mostrar = ['FECHA', 'HORA', 'ORIGEN', 'DESTINO']
 
-df_filtrado = df.dropna(subset=columnas_a_mostrar)
+# Filtrar solo los meses seleccionados: septiembre, enero, marzo, febrero y diciembre
+meses_seleccionados = [1, 2, 3, 9, 12]
+df_filtrado = df[df['Mes'].isin(meses_seleccionados)].copy()
+df_filtrado.loc[:, 'FECHA'] = df_filtrado['FECHA'].dt.strftime('%d-%m-%Y')
 
-horario_punta = df_filtrado[df_filtrado['HORA'].between(datetime.time(6, 0), datetime.time(8, 0)) |
-                            df_filtrado['HORA'].between(datetime.time(19, 0), datetime.time(23, 0))]
+horario_punta = df_filtrado[df_filtrado['HORA'].between(
+    '06:00', '08:00') | df_filtrado['HORA'].between('19:00', '23:00')]
+horario_bajo = df_filtrado[~df_filtrado['HORA'].between(
+    '06:00', '08:00') & ~df_filtrado['HORA'].between('19:00', '23:00')]
 
-horario_bajo = df_filtrado[~df_filtrado['HORA'].between(datetime.time(6, 0), datetime.time(8, 0)) &
-                           ~df_filtrado['HORA'].between(datetime.time(19, 0), datetime.time(23, 0))]
+total_viajes = len(df)
+print(f"Viajes en el archivo Excel original: {total_viajes}")
 
-guardar_excel(horario_punta[columnas_a_mostrar], 'viajes_horario_punta.xlsx')
-guardar_excel(horario_bajo[columnas_a_mostrar], 'viajes_horario_bajo.xlsx')
 
-print(f"Filas en el archivo Excel original: {len(df)}")
-print(f"Filas en el archivo Excel de horario punta: {len(horario_punta)}")
-print(f"Filas en el archivo Excel de horario bajo: {len(horario_bajo)}")
-print(
-    f"Filas filtradas (no incluidas en horario punta o bajo): {len(df) - len(horario_punta) - len(horario_bajo)}")
+viajes_punta = guardar_excel(
+    horario_punta[columnas_a_mostrar], 'horario_punta.xlsx')
+viajes_bajo = guardar_excel(
+    horario_bajo[columnas_a_mostrar], 'horario_bajo.xlsx')
+
+viajes_filtrados = total_viajes - (viajes_punta + viajes_bajo)
+print(f"Viajes eliminados: {viajes_filtrados}")
